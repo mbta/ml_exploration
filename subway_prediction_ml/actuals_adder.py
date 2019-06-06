09dd0ede69ffadd0ee720cefec5c72d4894d7c33
Phil Darnowsky <pdarnowsky@mbta.com>
2019-06-06 11:50:39 -0400

    GTFS trip IDs are only unique per service day

--- subway_prediction_ml/actuals_adder.py
+++ subway_prediction_ml/actuals_adder.py
@@ -14,26 +14,42 @@ class ActualsAdder(BaseEstimator, TransformerMixin):
     # Join actuals to a set of vehicle datapoints
     def transform(self, vehicle_datapoints, y=None):
         actuals = self._load_actuals()
+
         vehicle_datapoints["gtfs_trip_id"] = \
             vehicle_datapoints["gtfs_trip_id"].astype(str)
+        vehicle_datapoints["timestamp"] = \
+            vehicle_datapoints["timestamp"].apply(
+                lambda str: datetime.datetime.strptime(
+                    str,
+                    "%Y-%m-%dT%H:%M:%S.%fZ"
+                ).replace(tzinfo=pytz.UTC).timestamp()
+            )
+        self._add_service_date(actuals, 'time')
+        self._add_service_date(vehicle_datapoints, 'timestamp')
 
         merged_frame = vehicle_datapoints.merge(
             actuals,
             how="inner",
-            on='gtfs_trip_id'
-        )
-        merged_frame["timestamp"] = merged_frame["timestamp"].apply(
-            lambda str: datetime.datetime.strptime(
-                str,
-                "%Y-%m-%dT%H:%M:%S.%fZ"
-            ).replace(tzinfo=pytz.UTC).timestamp()
+            on=['gtfs_trip_id', 'service_date']
         )
         merged_frame["actual_seconds_from_now"] = \
             merged_frame["time"] - merged_frame["timestamp"]
-        merged_frame = merged_frame.drop(["timestamp", "time"], axis=1)
+        merged_frame = merged_frame.drop(
+            [
+                "timestamp",
+                "time",
+                'service_date'
+            ], axis=1
+        )
         merged_frame = merged_frame.query("actual_seconds_from_now > 0")
 
         return merged_frame.dropna()
+
+    def _add_service_date(self, dataframe, series_name):
+        dataframe['service_date'] = dataframe[series_name].apply(
+            self._service_date_for_timestamp
+        )
+        return dataframe
 
     # Build a dataframe from prediction analyzer logs, dropping actuals without
     # times, commuter rail trips, and duplicates (which we get because we log
@@ -54,3 +70,15 @@ class ActualsAdder(BaseEstimator, TransformerMixin):
         subway_actuals["gtfs_trip_id"] = \
             subway_actuals["gtfs_trip_id"].astype(str)
         return subway_actuals
+
+    def _service_date_for_timestamp(self, timestamp):
+        # Service days run 3 AM to 3 AM, so adjust the time by 3 hours
+        adjusted_timestamp = timestamp - 10800
+        utc_adjusted_time = datetime. \
+            datetime. \
+            utcfromtimestamp(adjusted_timestamp). \
+            replace(tzinfo=pytz.UTC)
+        adjusted_time = utc_adjusted_time.astimezone(
+            pytz.timezone('America/New_York')
+        )
+        return adjusted_time.toordinal()
